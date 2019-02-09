ad3fe20f704b8aeb55b9cda04e13baf63fff5524
h3yAlias <asthmaisback@gmail.com>
2019-02-09 19:18:56 +0300

    Пересобрал стартовый билд

--- coreplate/gulpfile.js
+++ coreplate/gulpfile.js
@@ -10,50 +10,50 @@ var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 ////////////////////////////////////
 gulp.task('sass', function() {
-    gulp.src('app/template/sass/**/*.scss')
-        .pipe(plumber())
-        .pipe(sass({outputStyle: 'expanded'}))
-        .pipe(autoprefixer())
-        .pipe(rename('style.css'))
-        .pipe(gulp.dest('app/template/css'))
-        .pipe(browserSync.reload({stream: true}))
+	gulp.src('app/template/sass/**/*.scss')
+		.pipe(plumber())
+		.pipe(sass({outputStyle: 'expanded'}))
+		.pipe(autoprefixer())
+		.pipe(rename('style.css'))
+		.pipe(gulp.dest('app/template/css'))
+		.pipe(browserSync.reload({stream: true}))
 });
 
 // Комментим эти строки //
 
 gulp.task('typescript', function() {
-    var tsProject = ts.createProject('tsconfig.json', { noImplicitAny: true });
-    var tsResult = gulp.src("app/template/ts/**/*.ts")
-        .pipe(tsProject());
+	var tsProject = ts.createProject('tsconfig.json', { noImplicitAny: true });
+	var tsResult = gulp.src("app/template/ts/**/*.ts")
+		.pipe(tsProject());
 
-    return tsResult.js.pipe(gulp.dest('app/template/js'));
+	return tsResult.js.pipe(gulp.dest('app/template/js'));
 });
 
 gulp.task('browserify', function() {
-    return browserify('./app/template/js/script.js')
-        .bundle()
-        .pipe(source('bundle.js'))
-        .pipe(gulp.dest('./app/template/js'));
+	return browserify('./app/template/js/script.js')
+		.bundle()
+		.pipe(source('bundle.js'))
+		.pipe(gulp.dest('./app/template/js'));
+
 });
 //////////////////////////////
 
 gulp.task('browser-sync', function() {
-    browserSync({
-        server: {
-            baseDir: 'app'
-        },
-        notify: false
-    });
+	browserSync({
+		server: {
+			baseDir: 'app'
+		},
+		notify: false
+	});
 });
 
 gulp.task('watch', ['browserify', 'browser-sync', 'sass'], function() {
-    gulp.watch('app/template/sass/**/*.scss', ['sass']);
-    // Комментим эти строки //
-     gulp.watch('app/template/ts/**/*.ts', ['typescript']);
-	   gulp.watch('app/template/js/**/*.js', ['browserify']);
-     //////////////////////////////
-    gulp.watch('app/template/js/**/*.js', browserSync.reload);
-    gulp.watch('app/*.html', browserSync.reload);
+	gulp.watch('app/template/sass/**/*.scss', ['sass']);
+	// Комментим эти строки //
+	gulp.watch('app/template/ts/**/*.ts', ['typescript', 'browserify']);
+	//////////////////////////////
+	gulp.watch('app/template/js/**/*.js', browserSync.reload);
+	gulp.watch('app/*.html', browserSync.reload);
 });
 
 gulp.task('default', ['watch']);
