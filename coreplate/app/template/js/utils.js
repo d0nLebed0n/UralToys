3288a365acf8cea081fe55c0e986db1a0377e4aa
Андрей Строгов <asthmaisback@gmail.com>
2019-06-19 03:27:39 +0300

    Header script

--- coreplate/app/template/js/utils.js
+++ coreplate/app/template/js/utils.js
@@ -1,41 +1,38 @@
 'use strict';
 
 (function () {
-  var checkVisibility = function (target) {
-    var targetPosition = {
-      top: window.pageYOffset + target.getBoundingClientRect().top,
-      left: window.pageXOffset + target.getBoundingClientRect().left,
-      right: window.pageXOffset + target.getBoundingClientRect().right,
-      bottom: window.pageYOffset + target.getBoundingClientRect().bottom
+  var mobileMenu = function() {
+    this._topMenuItem = document.querySelector('.header__top-list');
+    this._menuSubWrapper = document.querySelector('.js-detachElements');
+  };
+  mobileMenu.prototype.getCaseOfCurrentPoint = function() {
+    var menuWrapper = document.querySelector('.js-mobileWrapper'),
+      menuOverlay = document.querySelector('.js-mobileOverlay'),
+      menuBurger = document.querySelector('.js-openMobile');
+    if (window.matchMedia("(max-width:1440px)").matches === true) {
+      this._menuSubWrapper.appendChild(this._topMenuItem);
+      mobileMenu.prototype.setEventListeners({menuBurger, menuWrapper, menuOverlay})
     };
-
-
-    var windowPosition = {
-      top: window.pageYOffset,
-      left: window.pageXOffset,
-      right: window.pageXOffset + document.documentElement.clientWidth,
-      bottom: window.pageYOffset + document.documentElement.clientHeight
-    };
-
-    if (targetPosition.bottom > windowPosition.top &&
-			targetPosition.top < windowPosition.bottom &&
-			targetPosition.right <= windowPosition.right) {
-
-      return false;
-    } else {
-      return true;
-    }
   };
 
-  var ieCheck = function () {
-    var ua = navigator.userAgent;
-    var doc = document.documentElement;
-    if ((ua.match(/rv:11.0/i))) {
-      doc.className = doc.className + 'ie11';
-    }
+  mobileMenu.prototype.setEventListeners = function (props) {
+    const itemsList = {
+      burgerItem: props.menuBurger,
+      wrapperItem: props.menuWrapper,
+      overlayItem: props.menuOverlay
+    };
+    itemsList.burgerItem.addEventListener('click', function () {
+      this.classList.toggle('js-isActive');
+      itemsList.overlayItem.classList.toggle('js-isActive');
+      itemsList.wrapperItem.classList.toggle('js-isActive');
+    })
   };
+  document.addEventListener('DOMContentLoaded', function () {
+
+    var newMenu = new mobileMenu();
+    newMenu.getCaseOfCurrentPoint();
+  });
   window.utils = {
-    checkVis: checkVisibility,
     getIE: function () {
       var currentAgent = navigator.userAgent;
       if ((currentAgent.match(/rv:11.0/i))) {
