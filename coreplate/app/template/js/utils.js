d6d05b50feffbc7b53909f3609f9f2c6de3ae8f8
h3yAlias-_- <asthmaisback@gmail.com>
2019-06-20 03:05:12 +0300

    Мобильная шапки

--- coreplate/app/template/js/utils.js
+++ coreplate/app/template/js/utils.js
@@ -4,27 +4,63 @@
   var mobileMenu = function() {
     this._topMenuItem = document.querySelector('.header__top-list');
     this._menuSubWrapper = document.querySelector('.js-detachElements');
+    this._mainMenu = document.querySelector('.js-mainMenu');
   };
   mobileMenu.prototype.getCaseOfCurrentPoint = function() {
+
     var menuWrapper = document.querySelector('.js-mobileWrapper'),
       menuOverlay = document.querySelector('.js-mobileOverlay'),
-      menuBurger = document.querySelector('.js-openMobile');
+      menuBurger = document.querySelector('.js-openMobile'),
+      menuClose = document.querySelector('.js-closeMenu');
+
     if (window.matchMedia("(max-width:1440px)").matches === true) {
       this._menuSubWrapper.appendChild(this._topMenuItem);
-      mobileMenu.prototype.setEventListeners({menuBurger, menuWrapper, menuOverlay})
+      mobileMenu.prototype.setEventListeners({menuBurger, menuWrapper, menuOverlay, menuClose})
     };
+
+    if (window.matchMedia("(max-width:875px)").matches === true) {
+      this._menuSubWrapper.appendChild(this._mainMenu);
+      mobileMenu.prototype.hideUnusedElems(document.querySelectorAll('.js-mainMenu .navigation__link'));
+    };
+
+  };
+
+
+  mobileMenu.prototype.hideUnusedElems = function (props) {
+    console.log(props);
+    var listLinks = props;
+    listLinks.forEach(function (item) {
+      console.log(item.getAttribute('data-show'));
+      if (item.getAttribute('data-show') === 'false') {
+        item.parentNode.style.display = 'none';
+      }
+    })
   };
 
   mobileMenu.prototype.setEventListeners = function (props) {
     const itemsList = {
       burgerItem: props.menuBurger,
       wrapperItem: props.menuWrapper,
-      overlayItem: props.menuOverlay
+      overlayItem: props.menuOverlay,
+      closeItem: props.menuClose
     };
     itemsList.burgerItem.addEventListener('click', function () {
-      this.classList.toggle('js-isActive');
+      itemsList.burgerItem.classList.toggle('js-isActive');
       itemsList.overlayItem.classList.toggle('js-isActive');
       itemsList.wrapperItem.classList.toggle('js-isActive');
+      document.body.classList.toggle('js-overFlowed')
+    });
+    itemsList.closeItem.addEventListener('click', function () {
+      itemsList.burgerItem.classList.toggle('js-isActive');
+      itemsList.overlayItem.classList.toggle('js-isActive');
+      itemsList.wrapperItem.classList.toggle('js-isActive');
+      document.body.classList.toggle('js-overFlowed')
+    })
+    itemsList.overlayItem.addEventListener('click', function () {
+      itemsList.burgerItem.classList.toggle('js-isActive');
+      itemsList.overlayItem.classList.toggle('js-isActive');
+      itemsList.wrapperItem.classList.toggle('js-isActive');
+      document.body.classList.toggle('js-overFlowed')
     })
   };
   document.addEventListener('DOMContentLoaded', function () {
